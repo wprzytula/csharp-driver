4729f337b8d2e7f012ab97ea7d4a4576cde78e25
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-09-08 14:37:53 +0200

    add RustWrapper example
    The example can be used to experiment with using Rust code in C# via
    FFI. It's temporary and will be removed once not needed in development.
    It can be extended on the go with each new functionality that we already
    have reimplemented in the wrapper.

---
+++ examples/RustWrapper/Program.cs
@@ -0,0 +1,151 @@
+﻿//
+//       Copyright (C) DataStax Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using Cassandra;
+
+namespace RustWrapper
+{
+    /// <summary>
+    /// TODO: Add description
+    /// </summary>
+    internal class Program
+    {
+        private ICluster _cluster;
+        private ISession _session;
+
+        private static void Main(string[] args)
+        {
+            new Program().MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private async Task MainAsync(string[] args)
+        {
+            Console.WriteLine($"Beginning RustWrapper example!");
+
+            // build cluster
+            _cluster =
+                Cluster.Builder()
+                    .AddContactPoint("172.42.0.2")
+                    .WithLoadBalancingPolicy(new TokenAwarePolicy(new DCAwareRoundRobinPolicy("datacenter1")))
+                    .Build();
+
+            // create session via Rust FFI
+            var sessionPtr = await CreateSession("172.42.0.2").ConfigureAwait(false);
+
+            // Free session synchronously, not to cause a leak.
+            session_free(sessionPtr);
+
+            // create session
+            _session = await _cluster.ConnectAsync().ConfigureAwait(false);
+        }
+
+
+
+
+
+        // This shall be called by Rust code when the operation is completed.
+        //
+        // Signature in Rust: extern "C" fn(tcs: *mut c_void, res: *mut c_void)
+        //
+        // This attribute makes the method callable from native code.
+        // It also allows taking a function pointer to the method.
+        [UnmanagedCallersOnly(CallConvs = new Type[] { typeof(CallConvCdecl) })]
+        static void CompleteTask(IntPtr tcsPtr, IntPtr resPtr)
+        {
+            try
+            {
+                // Recover the GCHandle that was allocated for the TaskCompletionSource.
+                var handle = GCHandle.FromIntPtr(tcsPtr);
+
+                if (handle.Target is TaskCompletionSource<IntPtr> tcs)
+                {
+                    // Simply pass the opaque pointer back as the result.
+                    // The Rust code is responsible for interpreting the pointer's contents
+                    // and freeing it when no longer needed.
+                    tcs.SetResult(resPtr);
+
+                    // Free the handle so the TCS can be collected once no longer used
+                    // by the C# code.
+                    handle.Free();
+                }
+                else
+                {
+                    throw new InvalidOperationException("GCHandle did not reference a TaskCompletionSource<IntPtr>.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"[FFI] CompleteTask threw exception: {ex}");
+            }
+        }
+
+        [DllImport("csharp_wrapper", CallingConvention = CallingConvention.Cdecl)]
+        unsafe private static extern IntPtr session_create(IntPtr tcsPtr, IntPtr completeTask, string uri);
+        [DllImport("csharp_wrapper", CallingConvention = CallingConvention.Cdecl)]
+        unsafe private static extern void session_free(IntPtr sessionPtr);
+        private Task<IntPtr> CreateSession(string uri)
+        {
+            /**
+             * TaskCompletionSource is a way to programatically control a Task.
+             * We create one here and pass it to Rust code, which will complete it.
+             * This is a common pattern to bridge async code between C# and native code.
+             */
+            TaskCompletionSource<IntPtr> tcs = new(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            /**
+             * Although GC knows that it must not collect items during a synchronous P/Invoke call,
+             * it doesn't know that the native code will still require the TCS after the P/Invoke
+             * call returns.
+             * And tokio task in Rust will likely still run after the P/Invoke call returns.
+             * So, since we are passing the TCS to asynchronous native code, we need to pin it
+             * so it doesn't get collected by the GC.
+             * We must remember to free the handle later when the TCS is completed (see CompleteTask
+             * method).
+             */
+            var handle = GCHandle.Alloc(tcs);
+            IntPtr tcsPtr = GCHandle.ToIntPtr(handle);
+
+            // `unsafe` is required to get a function pointer to a static method.
+            // Note that we can get this pointer because the method is static and
+            // decorated with [UnmanagedCallersOnly].
+            unsafe
+            {
+                // This is the only way to get a function pointer to a method decorated
+                // with [UnmanagedCallersOnly] that I've found to compile.
+                delegate* unmanaged[Cdecl]<IntPtr, IntPtr, void> fnPtr = &CompleteTask;
+                IntPtr completeTaskPtr = (IntPtr)fnPtr;
+
+                // Invoke the native code, which will complete the TCS when done.
+                // We need to pass a pointer to CompleteTask because Rust code cannot directly
+                // call C# methods.
+                // Even though Rust code statically knows the name of the method, it cannot
+                // directly call it because the .NET runtime does not expose the method
+                // in a way that Rust can call it.
+                // So we pass a pointer to the method and Rust code will call it via that pointer.
+                // This is a common pattern to call C# code from native code ("reversed P/Invoke").
+                session_create(tcsPtr, completeTaskPtr, uri);
+            }
+
+            return tcs.Task;
+        }
+    }
+}
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,14 +1,86 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::ffi::{CStr, c_char, c_void};
+use std::future::Future;
+use std::marker::PhantomData;
+use std::ptr::NonNull;
+use std::sync::LazyLock;
+
+use scylla::client::session_builder::SessionBuilder;
+use tokio::runtime::Runtime;
+
+static RUNTIME: LazyLock<Runtime> = LazyLock::new(|| Runtime::new().unwrap());
+
+#[repr(transparent)]
+#[derive(Clone, Copy)]
+pub struct FfiPtr<'a, T: Sized> {
+    ptr: Option<NonNull<T>>,
+    _phantom: PhantomData<&'a ()>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+type CSharpStr<'a> = FfiPtr<'a, c_char>;
+impl<'a> CSharpStr<'a> {
+    fn as_cstr(&self) -> Option<&CStr> {
+        self.ptr.map(|ptr| unsafe { CStr::from_ptr(ptr.as_ptr()) })
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+enum Tcs {}
+
+#[repr(transparent)]
+pub struct TcsPtr(FfiPtr<'static, Tcs>);
+
+unsafe impl Send for TcsPtr {}
+
+type CompleteTask = unsafe extern "C" fn(tcs: TcsPtr, result: *mut c_void);
+
+#[unsafe(no_mangle)]
+pub extern "C" fn session_create(
+    tcs: TcsPtr,
+    complete_task: CompleteTask,
+    uri: CSharpStr<'_>,
+) {
+    // Convert the raw C string to a Rust string
+    let uri = uri.as_cstr().unwrap().to_str().unwrap();
+    let uri = uri.to_owned();
+
+    println!("Hello, World! {}", uri);
+
+    BridgedFuture::spawn(tcs, complete_task, async move {
+        println!("Create Session... {}", uri);
+        let session = SessionBuilder::new().known_node(&uri).build().await;
+        println!("Session created! {}", uri);
+        session
+    })
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn session_free(
+    session_ptr: *mut Session,
+) {
+    let _session = unsafe {Box::from_raw(session_ptr)};
+    println!("Session freed");
+    // Dropped here.
+}
+
+struct BridgedFuture {}
+
+impl BridgedFuture {
+    fn spawn<F, T>(tcs: TcsPtr, complete_task: CompleteTask, future: F)
+    where
+        F: Future<Output = T> + Send + 'static,
+        T: Send + 'static + std::fmt::Debug,
+    {
+        RUNTIME.spawn(async move {
+            let result = future.await;
+            // Here you would typically signal the completion to the C# side using `tcs`
+            println!(
+                "Future completed with result: {:?} - {:?}",
+                std::any::type_name::<T>(),
+                result
+            );
+            let boxed_result = Box::new(result);
+            let result_ptr = Box::into_raw(boxed_result) as *mut c_void;
+
+            unsafe { complete_task(tcs, result_ptr) };
+        });
     }
 }
