95f5e48be133b834b0bf721cf9e94113d8943993
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-09-16 15:27:34 +0200

    delete FrameParser

--- src/Cassandra/Connections/Connection.cs
+++ src/Cassandra/Connections/Connection.cs
@@ -806,7 +806,7 @@ namespace Cassandra.Connections
                         plainTextStream = compressor.Decompress(new WrappedStream(stream, header.BodyLength));
                         plainTextStream.Position = 0;
                     }
-                    response = FrameParser.Parse(new Frame(header, plainTextStream, serializer, resultMetadata));
+                    throw new NotImplementedException();
                 }
                 catch (Exception caughtException)
                 {
